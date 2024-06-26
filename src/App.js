b85fe093f279dd46815de2bf1b97658d9bf424bb
Nikhil0722 <55701103+Nikhil0722@users.noreply.github.com>
2024-06-27 00:02:24 +0530

    testing main branch

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
       <h1>I am the boss</h1>
       <h2>Clearing it</h2>
       <h1>This is pushed from main branch</h1>
+
+      <h1>This is pushed from main branch</h1>
     </div>
   );
 }
