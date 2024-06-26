e3d3eb789fc3a19ae8e2b5997c5160f76aec23f8
Nikhil0722 <55701103+Nikhil0722@users.noreply.github.com>
2024-06-27 00:11:42 +0530

    test

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>I am the boss</h1>
-      <h2>Clearing it</h2>
-      <h1>This is pushed from main branch</h1>
-
-      <h1>This is pushed from main branch</h1>
+    <h1>clear</h1>
     </div>
   );
 }
