d563939bb9f8aa600f72ec143997c13208e6eacc
Bhaskarkanishk <bhaskarkanishk@gmail.com>
2024-06-27 23:34:40 +0530

    bhaskar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <h1>To-Do List</h1>
       <h1>Kanishk</h1>
-=======
-      
->>>>>>> origin/master
+      <h2>uhoihoi</h2>
     </div>
   );
 }
